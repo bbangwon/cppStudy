663321188b636035896db2213216ad208c9caac6
bbangwon <bbangwon.chung@gmail.com>
2019-07-22 21:48:56 +0900

    Cpp Study

--- CppStudy/CppStudy5.cpp
+++ CppStudy/CppStudy5.cpp
@@ -5,10 +5,13 @@ class Marine {
 	int coord_x, coord_y;	//마린 위치
 	int damage;	//공격력
 	bool is_dead;
+	char *name;	// 마린 이름
 
 public:
 	Marine();	//기본 생성자
+	Marine(int x, int y, const char* marine_name);	//이름까지 지정
 	Marine(int x, int y);	//x,y 좌표에 마린 생성
+	~Marine();
 
 	int attack();	//데미지를 리턴한다.
 	void be_attack(int damage_earn);	//입는 데미지
@@ -20,6 +23,17 @@ public:
 Marine::Marine() {
 	hp = 50;
 	coord_x = coord_y = 0;
+	damage = 5;
+	is_dead = false;
+}
+
+Marine::Marine(int x, int y, const char *marine_name) {
+	name = new char[strlen(marine_name) + 1];
+	strcpy_s(name, strlen(marine_name) + 1, marine_name);
+
+	coord_x = x;
+	coord_y = y;
+	hp = 50;
 	damage = 5;
 	is_dead = false;
 }
@@ -50,9 +64,17 @@ void Marine::show_status() {
 	std::cout << " HP : " << hp << std::endl;
 }
 
+Marine::~Marine() {
+	std::cout << name << " 의 소멸자 호출 ! " << std::endl;
+	if (name != nullptr) {
+		delete[] name;
+	}
+}
+
 
 int main()
 {
+	/*
 	Marine marine1(2, 3);
 	Marine marine2(3, 5);
 
@@ -64,13 +86,14 @@ int main()
 
 	marine1.show_status();
 	marine2.show_status();
+	*/
 
 
 	// 배열
 	Marine* marines[100];
 
-	marines[0] = new Marine(2, 3);
-	marines[1] = new Marine(3, 5);
+	marines[0] = new Marine(2, 3, "Marine 2");
+	marines[1] = new Marine(3, 5, "Marine 1");
 
 	marines[0]->show_status();
 	marines[1]->show_status();
